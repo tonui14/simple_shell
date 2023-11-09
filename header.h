2678fc073cfda366133b635372015e901c5f7fdd
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-11-09 05:50:44 +0300

    Header file Update

--- header.h
+++ header.h
@@ -3,5 +3,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
 
 #endif
