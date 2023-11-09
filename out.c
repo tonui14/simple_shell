ac77fade6d3363251a9bdacc9fb1bc53dbe2ad61
Daisy Tonui <tonuidaisy@gmail.com>
2023-11-08 18:23:08 -0800

    out.c

---
+++ out.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+/**
+ * main = is the entry point
+ *
+ * Return: Shell $
+ **/
+
+int main()
+{
+	char *out = ( "(Shell)$");
+	{
+		printf("%s", out);
+		fflush(stdout);
+		return (0);
+	}
+}
+
