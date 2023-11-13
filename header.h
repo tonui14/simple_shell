ca4052f036526e2a0df34de2f57c48481c886a83
Daisy Tonui <tonuidaisy@gmail.com>
2023-11-13 10:11:35 -0800

    header.h

--- header.h
+++ header.h
@@ -13,5 +13,10 @@
 #include <unistd.h>
 #include <errno.h>
 
-void execute_command(const char *command);
+void shell_interactive(void);
+void shell_non_interactive (void);
+void execute_input_command(const char *input_command);
+void show_prompt(void);
+void get_user_input(char **input_command, size_t *size);
+void run_executable(const char *input_command);
 #endif
