5f22346173640d0b49702f72c24d3ee36b643be4
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-11-15 05:50:05 +0300

    Fixed Betty Errors in prompt.c file

--- prompt.c
+++ prompt.c
@@ -2,10 +2,11 @@
 
 /**
  * main - is the entry point of the code
- *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ * @envp: An array of strings containing the environment variables.
  * Return: 0
- **/
-
+ */
 int main(int argc, char **argv, char *envp[])
 {
 	char *input = NULL;
@@ -24,6 +25,13 @@ int main(int argc, char **argv, char *envp[])
 	return (0);
 }
 
+/**
+ * execute_input - Executes the given command.
+ * @command: An array of strings representing the command and its arguments.
+ * @envp: An array of strings containing the environment variables.
+ * Return: The status of the command execution.
+ */
+
 int execute_input(char **command, char *envp[])
 {
 	pid_t baby_pid;
@@ -33,19 +41,17 @@ int execute_input(char **command, char *envp[])
 
 	if (baby_pid < 0)
 	{
-		perror("fork");
-		
+		perror("fork")
 	}
 	else if (baby_pid == 0)
 	{
-	
 		if (execve(command[0], command, envp) == -1)
 		{
 			perror("execve");
 			exit(0);
 		}
 	}
-	else 
+	else
 	{
 		waitpid(baby_pid, NULL, 0);
 	}
